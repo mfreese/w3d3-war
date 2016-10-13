624c4215441340dc2ab52622f6ef5813bbc4b830
Michael Freese <mrfreese1@gmail.com>
2016-10-12 23:03:16 -0400

    submit

--- card.rb
+++ card.rb
@@ -2,32 +2,27 @@ class Card
     attr_accessor :face,
                   :suit,
                   :value
-    def initialize
+    def initialize (face, suit)
         self.face = face
         self.suit = suit
-        self.value = value
+        face_value
     end
-    def suit
-        %w(hearts clubs diamonds spades)
+    def suit_list
+        %w(Hearts Clubs Diamonds Spades)
     end
 
-    def face
+    def face_list
       %w(2 3 4 5 6 7 8 9 10 J Q K A)
     end
-    def value
+    def value_list
         %w(2 3 4 5 6 7 8 9 10 11 12 13 14)
     end
-
-    suit = %w(hearts clubs diamonds spades)
-
-    face = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-
     def face_value
       if
         face == "A"
         self.value = 14
         elsif
-          face== "K"
+          face == "K"
         self.value = 13
       elsif
         face == "Q"
@@ -39,5 +34,7 @@ class Card
         self.value = face_to.i
       end
     end
+  end
 
-end
+  # card_one = Card.new ("J", "Hearts")
+  # puts card_one.inspect
--- game.rb
+++ game.rb
@@ -1,12 +1,21 @@
-require_relative 'card.rb'
+require_relative 'deck'
 
 class WAR
 
-  attr_accessor :game
+  attr_accessor :game, :player_card, :computer_card,
 
-  
+  initialize
+  self.player_card = 52.times.collect
+  self.computer_card = 52.times.collect
+
+
 
   def play
-    say_hello
+    say_lets_play_war
+    player
+    computer
     rematch?
+
+    puts "Lets play War"
   end
+end
